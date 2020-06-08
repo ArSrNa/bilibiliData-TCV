0f96326130def30b17360beff9185c6030978632
Ar-Sr-Na <42886855+ArSrNa@users.noreply.github.com>
2020-06-08 14:01:11 +0800

    Update fans.php

--- fans.php
+++ fans.php
@@ -4,6 +4,7 @@ $json= file_get_contents('https://api.bilibili.com/x/relation/stat?vmid=24749747
 $array= array(json_decode($json));
 $ss = array_column($array, 'data');
 $jsona= json_encode($ss);
+echo($final);
 return($final);
 }
 ?>
