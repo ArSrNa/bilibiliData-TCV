33ce72eb77c7aaa9c14abe890b65c4814d433f2a
Ar-Sr-Na <42886855+ArSrNa@users.noreply.github.com>
2020-06-08 13:59:55 +0800

    适应云函数
    适应云函数

--- fans.php
+++ fans.php
@@ -1,9 +1,9 @@
 <?php
+function main_handler($event, $context) {
 $json= file_get_contents('https://api.bilibili.com/x/relation/stat?vmid=24749747');
 $array= array(json_decode($json));
 $ss = array_column($array, 'data');
 $jsona= json_encode($ss);
-$a= str_replace("{","[{",$json);
-$b= str_replace("}","}]",$a);
-print($jsona);
+return($final);
+}
 ?>
