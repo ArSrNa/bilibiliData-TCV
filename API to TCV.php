a273d3b972a6052690052d111fbc782680f27393
Ar-Sr-Na <42886855+ArSrNa@users.noreply.github.com>
2020-06-09 13:18:45 +0800

    优化变量
    把杂乱的API整合，添加注释

---
+++ API to TCV.php
@@ -0,0 +1,11 @@
+<?php
+//如果是云函数要加function main_handler($event, $context) {
+//输入API地址
+$url= "https://api.bilibili.com/x/space/acc/info?mid=24749747"
+$json= file_get_contents($url);
+$array= array(json_decode($json));
+$ss = array_column($array, 'data');
+$jsona= json_encode($ss);
+print_r($jsona);
+//}
+?>
---
+++ SESSDATA.php
@@ -0,0 +1,20 @@
+<?php
+$ch =curl_init();
+//此处输入API的URL
+$url= "http://member.bilibili.com/x/web/data/pandect?type=1"
+curl_setopt($ch,CURLOPT_URL,$url);
+//此处输入浏览器Cookie中的SESSDATA，保密！
+$sessdata= 
+curl_setopt($ch,CURLOPT_COOKIE,$sessdata);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+
+//$json= file_get_contents('https://www.arsrna.cn/bilibilidata/ccc.php');
+$array= array(json_decode($response,true));
+$sss = array_column($array, 'data');
+$jsona= json_encode($sss);
+$fin= str_replace("[[","[",$jsona);
+$finb= str_replace("]]","]",$fin);
+print_r($finb);
+//}
+?>
