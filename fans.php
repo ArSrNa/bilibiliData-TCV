30aa5994b9e32e1a6712285690629918946461fa
Ar-Sr-Na <42886855+ArSrNa@users.noreply.github.com>
2020-06-09 13:19:12 +0800

    Delete fans.php

--- fans.php
+++
@@ -1,9 +0,0 @@
-<?php
-//如果是云函数要加function main_handler($event, $context) {
-$json= file_get_contents('https://api.bilibili.com/x/relation/stat?vmid=24749747');
-$array= array(json_decode($json));
-$ss = array_column($array, 'data');
-$jsona= json_encode($ss);
-print_r($jsona);
-//}
-?>
